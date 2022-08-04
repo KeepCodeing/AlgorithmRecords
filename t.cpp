26e81d43b82df1fa40a829823d047ae341e32176
KeepCodeing <3350680655@qq.com>
2022-08-04 08:54:02 +0800

    remove mp4 files

--- t.cpp
+++ t.cpp
@@ -3,27 +3,25 @@ using namespace std;
 
 const int N = 110, M = 1110;
 
-struct Node {
-	int w, v;
-	Node(int w1, int v1): w(w1), v(v1) {};
-	Node() {};
-};
 
-Node node[N];
 
 long long f[M];
+int num[M];
 
 
 int main() {
-	int n, m;
-	cin >> n >> m;
-	for (int i = 1; i <= n; i++) cin >> node[i].w >> node[i].v;
+	int n ;
+	cin >> n;
+	for (int i = 1; i <= n; i++) cin >> num[i];
 	
 	for (int i = 1; i <= n; i++) {
-		for (int j = m; j >= node[i].w; j--) {
-			f[j] += f[j - node[i].w];
+		f[i] = 1;
+		for (int j = 1; j < i; j++) {
+			if (f[i] > f[j]) {
+				f[j] = max(f[i], f[j] + 1);
+			}
 		}
 	}
-	cout << f[m] << endl;
+	
 	return 0;
 }
